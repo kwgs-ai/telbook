f6dc37d3ed7099860f3861c9c38142ac99a744f3
Ai Kawagishi <ne201062@senshu-u.jp>
2021-11-06 16:49:59 +0000

    Tel削除作成

--- app/controllers/telephones_controller.rb
+++ app/controllers/telephones_controller.rb
@@ -3,20 +3,26 @@ class TelephonesController < ApplicationController
   end
 
   def show
+
   end
 
   def new
     @friend = Friend.find(session[:friend_id])
-    @telphone = Telephone.new(number: params[:q], cellphone: params[:cell])
-    @telphone.author = current_member
+    @telephone = Telephone.new(number: params[:q], cellphone: params[:cell])
+    @telephone.author = current_member
 
-    if @telphone.save
+    if @telephone.save
       redirect_to @friend, notice: "追加しました"
     else
       render "friends/show"
     end
   end
-
+  def destroy
+    @friend = Friend.find(session[:friend_id])
+    @telephone = current_member.telephones.find(params[:id])
+    @telephone.destroy
+    redirect_to @friend, notice: "TELを削除しました。"
+  end
   def edit
   end
   def create
