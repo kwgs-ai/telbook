73af4037eab9125ee36826c424e2c4809f1bdcdb
Ai Kawagishi <ne201062@senshu-u.jp>
2021-11-06 06:28:53 +0000

    routing設定

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,22 @@ class FriendsController < ApplicationController
   def index
     @friends = Friend.all
   end
+  def show
+
+  end
+  def new
+
+  end
+  def edit
+
+  end
+  def ceate
+
+  end
+  def update
+
+  end
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "friends#index"
+  resources :friends
 
 
 end
